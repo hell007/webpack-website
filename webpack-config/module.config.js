a8eeb5ebaa7745628227b747ddd8fdcd19df4ef8
hell007 <1048523306@qq.com>
2017-07-05 15:17:15 +0800

    修改css 支持热更新

--- webpack-config/module.config.js
+++ webpack-config/module.config.js
@@ -1,66 +1,75 @@
 const path = require('path');
 const ROOT = process.cwd(); // 根目录   /webpack-website
+const IsDev = process.env.NODE_ENV === 'dev' ? true : false;
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const srcDir = ROOT + "/src";
 
-module.exports = {
-	rules: [{
-			test: /\.(less|css)$/,
-			use: ExtractTextPlugin.extract({
-				use: [{
-						loader: 'css-loader',
-					},
-					{
-						loader: 'less-loader',
-					},
-				],
-				fallback: 'style-loader',
-			}),
-		},
-		{
-			test: /\.js$/,
-			include: srcDir,
-			loader: 'babel-loader',
-			options: {
-				presets: [
-					['es2015', {
-						loose: true
-					}]
-				],
-				cacheDirectory: true,
-				plugins: ['transform-runtime'],
+//dev下不合并css,以此来支持热更新
+let cssRules = IsDev ? {test: /\.(less|css)$/,use: ['style-loader', 'css-loader', 'less-loader']} : {
+	test: /\.(less|css)$/,
+	use: ExtractTextPlugin.extract({
+		use: [{
+				loader: 'css-loader',
 			},
+			{
+				loader: 'less-loader',
+			},
+		],
+		fallback: 'style-loader',
+	}),
+};
+
+//设置moudle rules
+let configRules = [
+	{
+		test: /\.js$/,
+		include: srcDir,
+		loader: 'babel-loader',
+		options: {
+			presets: [
+				['es2015', {
+					loose: true
+				}]
+			],
+			cacheDirectory: true,
+			plugins: ['transform-runtime'],
 		},
-		{
-			// 图片加载器，可以将较小的图片转成base64，减少http请求  
-			// 如下配置，将小于30720byte的图片转成base64码
-			// 只针对样式背景图片
-			test: /\.(png|jpg|gif)$/,
-			use: [{
-				loader: 'url-loader',
-				options: {
-					limit: 30720, //将小于30720byte图片转base64。设置图片大小，小于此数则转换
-					publicPath: '',
-					name: 'assets/img/[name].[ext]?[hash:8]' //输出目录以及名称
-				}
-			}]
-		},
-		{
-			//加载字体文件图标
-			test: /\.(woff|ttf|eot|svg)$/,
-			use: [{
-				loader: 'url-loader',
-				options: {
-					limit: 10000,
-					publicPath: '',
-					name: 'font/[name].[ext]?[hash:8]' //这里打包会生成一个在dist or build 下的 font/
-				}
-			}]
-		},
-		{
-			test: /\.html$/,
-			include: srcDir,
-			loader: 'html-loader',
-		},
-	]
+	},
+	{
+		// 图片加载器，可以将较小的图片转成base64，减少http请求  
+		// 如下配置，将小于30720byte的图片转成base64码
+		// 只针对样式背景图片
+		test: /\.(png|jpg|gif)$/,
+		use: [{
+			loader: 'url-loader',
+			options: {
+				limit: 30720, //将小于30720byte图片转base64。设置图片大小，小于此数则转换
+				publicPath: '',
+				name: 'assets/img/[name].[ext]?[hash:8]' //输出目录以及名称
+			}
+		}]
+	},
+	{
+		//加载字体文件图标
+		test: /\.(woff|ttf|eot|svg)$/,
+		use: [{
+			loader: 'url-loader',
+			options: {
+				limit: 10000,
+				publicPath: '',
+				name: 'font/[name].[ext]?[hash:8]' //这里打包会生成一个在dist or build 下的 font/
+			}
+		}]
+	},
+	{
+		test: /\.html$/,
+		include: srcDir,
+		loader: 'html-loader',
+	},
+];
+
+configRules.push(cssRules);
+
+module.exports = {
+	rules: configRules
 };
