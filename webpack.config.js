75363dc91261a8b37b899818a8a41494dcba5537
hell007 <1048523306@qq.com>
2017-07-05 16:48:36 +0800

    dev与product整合

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const IsDev = process.env.NODE_ENV === 'dev' ? true : false;
+const plugins = IsDev ? require('./webpack-config/plugins.dev.config.js') : require('./webpack-config/plugins.product.config.js');
+
 module.exports = {
 	entry: require('./webpack-config/entry.config.js'),
 	output: require('./webpack-config/output.config.js'),
 	module: require('./webpack-config/module.config.js'),
 	resolve: require('./webpack-config/resolve.config.js'),
-	plugins: require('./webpack-config/plugins.product.config.js'),
+	plugins: plugins,
 	externals: require('./webpack-config/externals.config.js')
 };
