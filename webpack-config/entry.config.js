2461b84ded59ca1a1d37badab53eb7910a6f7c92
hell007 <1048523306@qq.com>
2017-07-05 16:55:51 +0800

    Update entry.config.js

--- webpack-config/entry.config.js
+++ webpack-config/entry.config.js
@@ -2,3 +2,10 @@ const {getEntry} = require('./utils.config.js'); //引入工具模块
 
 
 module.exports = getEntry('./src/js/**/*.js');
+
+
+//module.exports = {
+//  index: './src/js/index.js', 
+//  list: './src/js/list.js',
+//  about: './src/js/about.js'
+//}
